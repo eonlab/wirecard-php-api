21625eb8ad362e3a9e325e991a8aa4315596862b
Igor Gaponov <jiminy96@gmail.com>
2015-05-01 09:53:19 +0200

    added tes for Response::isSuccessful()

--- tests/Element/ResponseTest.php
+++ tests/Element/ResponseTest.php
@@ -4,14 +4,53 @@ namespace Wirecard\Element;
 
 class ResponseTest extends \PHPUnit_Framework_TestCase
 {
-    public function testRequestHasJob()
+    public function testResponseHasJob()
     {
-        $job = $this->getMockBuilder('\Wirecard\Element\Job')
+        $job = $this->getJobMock();
+
+        $response = new Response($job);
+        $this->assertInstanceOf('\Wirecard\Element\Job', $response->job);
+    }
+
+    public function testIsSuccessfulReturnsFalseOnError()
+    {
+        $job = $this->getJobMock();
+
+        $response = new Response($job);
+        $response->error = true;
+        $this->assertFalse($response->isSuccessful());
+    }
+
+    public function testIsSuccessfulReturnsFalseOnJobError()
+    {
+        $job = $this->getJobMock();
+        $job->error = true;
+
+        $response = new Response($job);
+        $this->assertFalse($response->isSuccessful());
+    }
+
+    public function testIsSuccessfulReturnsTransactionResult()
+    {
+        $job = $this->getJobMock(['getTransaction']);
+        $transaction = $this->getMock('\Wirecard\Element\Transaction', ['isSuccessful']);
+        $transaction->expects($this->once())->method('isSuccessful')->will($this->returnValue(true));
+        $job->expects($this->once())->method('getTransaction')->will($this->returnValue($transaction));
+
+        $response = new Response($job);
+        $this->assertTrue($response->isSuccessful());
+    }
+
+    /**
+     * @param array $methods
+     * @return \PHPUnit_Framework_MockObject_MockObject|Job
+     */
+    protected function getJobMock($methods = [])
+    {
+        return $this->getMockBuilder('\Wirecard\Element\Job')
+            ->setMethods($methods)
             ->disableOriginalConstructor()
             ->getMock();
-
-        $request = new Response($job);
-        $this->assertInstanceOf('\Wirecard\Element\Job', $request->job);
     }
 }
  
