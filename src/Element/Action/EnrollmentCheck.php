fabd59739d0dca360625405b3d500117f9fd3ee2
eonlab <luisfsd@tecbit.eu>
2019-05-25 21:02:22 +0100

    Update EnrollmentCheck.php

--- src/Element/Action/EnrollmentCheck.php
+++ src/Element/Action/EnrollmentCheck.php
@@ -6,6 +6,13 @@ use Wirecard\Element\AbstractAction;
 
 class EnrollmentCheck extends AbstractAction
 {
+    const TYPE_PAYMENT = 'payment';
+    const TYPE_NONPAYMENT = 'non-payment';
+  
+    const VERSION_ONE = '1.0';
+    const VERSION_TWO = '2.1';
 
+    public $payment = self::TYPE_PAYMENT;
+    public $version = self::VERSION_TWO;
 }
  
