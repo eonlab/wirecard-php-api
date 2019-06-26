ed9784ddc354f12bf2e2b1d757a3054ce03f8de3
eonlab <luisfsd@tecbit.eu>
2019-06-26 21:43:18 +0100

    Update EnrollmentCheck.php

--- src/Element/Action/EnrollmentCheck.php
+++ src/Element/Action/EnrollmentCheck.php
@@ -9,10 +9,10 @@ class EnrollmentCheck extends AbstractAction
     const TYPE_PAYMENT = 'payment';
     const TYPE_NONPAYMENT = 'non-payment';
   
-    const VERSION_ONE = '1.0';
+    const VERSION_ONE = '1.0.2';
     const VERSION_TWO = '2.1';
 
     public $type = self::TYPE_PAYMENT;
-    public $version = self::VERSION_TWO;
+    public $version = self::VERSION_ONE;
 }
  
